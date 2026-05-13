cb4a2718b36f805fcd685c082ae70116c0c5a3a6
ALMA'RUF HIDAYAT <hidayatmaruf99@gmail.com>
2026-05-13 16:59:32 +0800

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,11 @@
+let bintang = 1
+for ( let x = 1; x < 5; x++ ) {
+    let cetak = "";
+    for ( let y = 1; y <= bintang; y++ ) {
+        cetak += "*"
+    }
+    console.log(cetak);
+    bintang +=1
+}
+
+

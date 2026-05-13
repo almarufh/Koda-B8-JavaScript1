e4307f5ced43b957a40ba7505c244c272d60ac95
ALMA'RUF HIDAYAT <hidayatmaruf99@gmail.com>
2026-05-13 11:31:44 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const jariJari = 14;
+const phi = 22/7;
+let luasLingkaran = 0;
+let kelilingLingkaran = 0;
+
+luasLingkaran = phi * jariJari * jariJari;
+kelilingLingkaran = 2 * phi * jariJari;
+
+
+console.log(luasLingkaran);
+console.log(kelilingLingkaran);
